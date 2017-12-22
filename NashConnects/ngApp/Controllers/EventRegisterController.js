68aa9fc8735e2d03082f30f09010412fd74c1d08
SMITHsharon <sharon.e.smith2@gmail.com>
2017-12-21 19:32:44 -0800

    Event Registration screen info displaying

--- NashConnects/ngApp/Controllers/EventRegisterController.js
+++ NashConnects/ngApp/Controllers/EventRegisterController.js
@@ -12,32 +12,47 @@
     vm.message = "Event Registration";
 
     $scope.thisProfile = {};
-    $scope.thisNonProfit = {};
     $scope.thisEvent = {};
+    $scope.nonProfitName;
 
-    // get user info
+    // get NonProfit name
+    $http.get(`/api/NonProfits/${nonprofitId}`)
+        .then((npResult) => {
+            var nonProfitResult = npResult.data;
+            console.log("nonProfitResult.Name :: ", nonProfitResult.Name);
+            $scope.nonProfitName = nonProfitResult.Name;
+            var nonProfitId = nonProfitResult.Id;
+            
+        }).catch((npError) => {
+            console.log("error, getting NonProfit Name :: ", npError);
+        });
+
+
+    // get User info
     $http.get("/api/Freelancers/current")
         .then((result) => {
-            console.log("result.data", result.data);
             $scope.thisProfile = result.data;
-            userid = result.data.Id;
-            console.log("userid :: ", userid);
+            userId = result.data.Id;
         })
         .catch((getUserError) => {
             console.log("getFreelancerProfile", getUserError);
         });
 
-    // get event info
-    //$http.get(`/api/Events/${eventId}`)
-    //$http.get(`/api/NonProfits/${nonprofitId}/events/list`)
+
+    // get Event info
     $http.get(`/api/NonProfits/${nonprofitId}/events/${eventId}`)
         .then((eventResult) => {
-            console.log("eventResult :: ", eventResult);
-            console.log("eventResult.data :: ", eventResult.data);
             $scope.thisEvent = eventResult.data;
         })
         .catch((eventError) => {
             console.log("error on getEvent", eventError);
         });
+
+    $scope.postRegistration = (userId, eventId) => {
+        console.log("passing userId, eventId :: ", userId, eventId);
+        // display confirmation message
+        // on OK
+        $location.url(`/nonprofit/${nonprofitId}/events/list`)
+    }
 
 }]);
