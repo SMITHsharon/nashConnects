8dc5da7f529fffab2b5ac5849ffb2cb93830f7c6
SMITHsharon <sharon.e.smith2@gmail.com>
2017-12-27 22:53:50 -0800

    List Newsletter Subscribers appear in ng-Inspector, but not rendering to browser ?

--- NashConnects/ngApp/Controllers/newsletterListController.js
+++ NashConnects/ngApp/Controllers/newsletterListController.js
@@ -11,20 +11,26 @@
     vm.message = "Subscribed to Newsletter";
 
     $scope.freelancers = [];
+    listOfFreelancers = [];
 
     var getFreelancerList = () => {
         $http.get("/api/Freelancers/list/newsletter")
             .then((result) => {
-                var dataResults = result.data;
-                var listOfFreelancers = [];
+                listOfFreelancers = result.data;
+                console.log("listOfFreelancers :: ", listOfFreelancers);
+                console.log("listOfFreelancers[0] :: ", listOfFreelancers[0]);
 
+                /*
                 if (dataResults.length > 0) {
                     Object.keys(dataResults).forEach((key) => {
                         dataResults[key].id = key;
                         listOfFreelancers.push(dataResults[key]);
                     });
                 }
+                */
+
                 $scope.freelancers = listOfFreelancers;
+                console.log("$scope.freelancers :: ", $scope.freelancers);
 
             }).catch((error) => {
                 console.log("error, listing all Freelancers :: ", error);
